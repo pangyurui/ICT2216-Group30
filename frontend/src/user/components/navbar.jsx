833a88866ea8adec67d4ca4620dcc1e0ccc3ed6b
nickphoon <potatonicholas@gmail.com>
2024-07-05 00:06:23 +0800

    added admindashboard, updated navigation and added payments

--- frontend/src/user/components/navbar.jsx
+++ frontend/src/user/components/navbar.jsx
@@ -1,17 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { ShoppingCart } from "phosphor-react";
+import { ShoppingCart, UserCircle } from "phosphor-react";
+import Swal from "sweetalert2";
 import "./navbar.css";
 
-export const Navbar = () => {
+export const Navbar = ({ auth, logout }) => {
+  const handleLogout = () => {
+    logout();
+    Swal.fire(
+      'Logged Out',
+      'You have been logged out successfully.',
+      'success'
+    );
+  };
+
   return (
     <div className="navbar">
       <div className="links">
         <Link to="/"> Shop </Link>
         <Link to="/contact"> Contact </Link>
+        <Link to="/aboutus"> About us </Link>
         <Link to="/cart">
           <ShoppingCart size={32} />
         </Link>
+      </div>
+      <div className="profile">
+        <UserCircle size={36} /> 
+        {auth ? (
+          <div className="account-options">
+            {/* <p>Hello, {auth.username}</p> */}
+            <Link to="/account">Account</Link>
+            <Link to="/payments">Payment Methods</Link>
+            <Link to="/addresses">Addresses</Link>
+            <Link to="/" onClick={handleLogout}>Logout</Link>
+          </div>
+        ) : (
+          <div className="auth-links">
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </div>
+        )}
       </div>
     </div>
   );
