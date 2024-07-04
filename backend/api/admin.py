ce277f58bf677d543ae065ccdc61374ab16147a9
pangyurui <rui2gen@gmail.com>
2024-07-05 00:35:55 +0800

    update admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
-from .models import Product, ProductCategory, Organisation
-
-# Register your models here
-@admin.register(ProductCategory)
-class ProductCategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'created_at', 'modified_at', 'deleted_at')
-    list_filter = ('created_at', 'modified_at', 'deleted_at')
+from .models import Product, Organisation
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'category_id', 'created_at', 'modified_at', 'deleted_at')
+    list_display = ('name', 'price', 'created_at', 'modified_at', 'deleted_at')
     list_filter = ('created_at', 'modified_at', 'deleted_at')
 
 @admin.register(Organisation)
