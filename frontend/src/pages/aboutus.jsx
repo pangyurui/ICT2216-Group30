c0192e4c400eaedafe0394f94a72e06517bd8082
nickphoon <potatonicholas@gmail.com>
2024-07-05 00:18:04 +0800

    add about us

---
+++ frontend/src/pages/aboutus.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './aboutus.css'; 
+
+export const AboutUs = () => {
+    return (
+        <div className="about-container">
+            <h1>About Us</h1>
+            <p>
+                At CharityCentral, we are driven by a single goal: to make giving back to the community as easy and impactful as possible. Our platform responds to the increasing global need for a unified response to humanitarian crises, such as the Israeli-Palestinian conflict and the Russo-Ukrainian War, as well as ongoing issues in animal welfare, childcare, and environmental conservation.
+            </p>
+            <p>
+                CharityCentral is an all-in-one charity platform designed to empower users and organizations to support causes they are passionate about through donations and product sales. Our platform simplifies charitable giving by centralizing it, making it convenient for users to make a difference and ensuring that contributions reach their intended recipients swiftly and securely.
+            </p>
+            <p>
+                Together, we work to create a world where support and solidarity can cross borders effortlessly.
+            </p>
+            <p>
+                Join us in making a difference today. Whether you're looking to contribute to urgent global issues or support local causes, CharityCentral makes it possible for your generosity to have the maximum impact. Explore our platform to learn more about how you can contribute to the causes closest to your heart.
+            </p>
+        </div>
+    );
+};
