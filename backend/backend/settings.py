a490551e0d6c013e3378d8fa4bacacbd5b5592b9
RI0US04 <121922214+RI0US04@users.noreply.github.com>
2024-07-06 00:11:35 +0800

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -148,10 +148,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 # #CSRF settings
 CORS_ALLOW_CREDENTIALS = True
 CSRF_COOKIE_SECURE = True
