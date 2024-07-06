e7e660ba52a0607c6a926e35af8a89f11e924418
nickphoon <potatonicholas@gmail.com>
2024-07-06 16:55:26 +0800

    added UI test and Unit testing

---
+++ backend/api/tests/test_views.py
@@ -0,0 +1,131 @@
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from unittest.mock import patch
+from api.models import User, Product, Organisation, Cart, CartItem
+import json
+
+class BaseTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = APIClient()
+        cls.user_data = {
+            'username': 'testuser',
+            'email': 'testuser@example.com',
+            'password': 'testpassword',
+            'first_name': 'Test',
+            'last_name': 'User'
+        }
+        cls.superuser_data = {
+            'username': 'adminuser',
+            'email': 'admin@example.com',
+            'password': 'adminpassword',
+            'first_name': 'Admin',
+            'last_name': 'User'
+        }
+
+        # Create normal user
+        cls.user = User.objects.create_user(**cls.user_data)
+
+        # Mock the OTP verification process
+        with patch('pyotp.TOTP.verify', return_value=True), \
+             patch('builtins.input', return_value='123456'), \
+             patch('qrcode.QRCode.print_ascii', return_value=None):  # Mock the QR code printing
+                cls.superuser = User.objects.create_superuser(**cls.superuser_data)
+
+    def setUp(self):
+        self.client = APIClient()  # Ensure APIClient is used here
+
+    def get_csrf_token(self):
+        response = self.client.get(reverse('get_csrf_token'))
+        return response.cookies['csrftoken'].value
+
+    def authenticate(self, user_data):
+        login_data = {
+            'username': user_data['username'],
+            'password': user_data['password'],
+            'otp': '123456'  # This is the mocked OTP value
+        }
+        csrf_token = self.get_csrf_token()
+        response = self.client.post(reverse('login'), data=json.dumps(login_data), content_type='application/json', headers={'X-CSRFToken': csrf_token})
+        
+        # Check if login was successful and tokens are present
+        if response.status_code != status.HTTP_200_OK:
+            print("Login failed with status code:", response.status_code)
+            print("Response content:", response.content)
+            return None
+        
+        if 'access' not in response.data or 'refresh' not in response.data:
+            print("Access or refresh token missing in the response")
+            print("Response data:", response.data)
+            return None
+        
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + response.data['access'])
+        return True
+
+class UserTests(BaseTestCase):
+    def test_user_registration(self):
+        new_user_data = {
+            'username': 'newtestuser',
+            'email': 'newtestuser@example.com',
+            'password': 'newtestpassword',
+            'first_name': 'NewTest',
+            'last_name': 'User'
+        }
+        csrf_token = self.get_csrf_token()
+        response = self.client.post(reverse('register'), data=json.dumps(new_user_data), content_type='application/json', headers={'X-CSRFToken': csrf_token})
+        if response.status_code != status.HTTP_201_CREATED:
+            print("Registration response content:", response.content)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        print("test_user_registration passed.")
+
+    @patch('pyotp.TOTP.verify', return_value=True)
+    def test_user_login(self, mock_verify):
+        login_data = {
+            'username': self.superuser_data['username'],
+            'password': self.superuser_data['password'],
+            'otp': '123456'  # This is the mocked OTP value
+        }
+        csrf_token = self.get_csrf_token()
+        response = self.client.post(reverse('login'), data=json.dumps(login_data), content_type='application/json', headers={'X-CSRFToken': csrf_token})
+        if response.status_code != status.HTTP_200_OK:
+            print("Login response content:", response.content)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
+        print("test_user_login passed.")
+
+class OrganisationTests(BaseTestCase):
+    @patch('pyotp.TOTP.verify', return_value=True)
+    def test_create_organisation_as_admin(self, mock_verify):
+        self.authenticate(self.superuser_data)
+        
+        organisation_data = {
+            'name': 'Test Organisation',
+            'desc': 'A test organisation',
+            'created_at': '2024-07-06T00:00:00Z',
+            'modified_at': '2024-07-06T00:00:00Z'
+        }
+        response = self.client.post(reverse('organisation-create'), data=organisation_data)
+        print("Admin create organisation response:", response.json())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.json()['name'], 'Test Organisation')
+        print("test_create_organisation_as_admin passed.")
+
+    @patch('pyotp.TOTP.verify', return_value=True)
+    def test_create_organisation_as_normal_user(self, mock_verify):
+        self.authenticate(self.user_data)
+        
+        organisation_data = {
+            'name': 'Test Organisation',
+            'desc': 'A test organisation',
+            'created_at': '2024-07-06T00:00:00Z',
+            'modified_at': '2024-07-06T00:00:00Z'
+        }
+        response = self.client.post(reverse('organisation-create'), data=organisation_data)
+        print("Normal user create organisation response:", response.json())
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertIn('detail', response.json())
+        self.assertEqual(response.json()['detail'], 'You cannot perform this action')
+        print("test_create_organisation_as_normal_user passed.")
---
+++ testUi.py
@@ -0,0 +1,205 @@
+import unittest
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pyotp
+import time
+import random
+import string
+import cv2
+from PIL import Image
+from pyzbar.pyzbar import decode
+import urllib.parse as urlparse
+from urllib.parse import parse_qs
+from django.conf import settings
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "backend.backend.settings"
+def generate_random_username(length=8):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
+def generate_random_password(length=12):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for i in range(length))
+
+def generate_random_email(username_length=8, domain="example.com"):
+    username = generate_random_username(username_length)
+    return f"{username}@{domain}"
+
+class UITest(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        # Set up WebDriver (Chrome in this example)
+        cls.driver = webdriver.Chrome()  # Ensure chromedriver is in PATH or provide the correct path
+        cls.driver.implicitly_wait(10)
+        cls.driver.maximize_window()
+        cls.host = settings.CSRF_TRUSTED_ORIGINS[2]
+        cls.qr_code_image_path = 'qrcode.png'
+        cls.totp = None
+        cls.username = generate_random_username()
+        cls.password = generate_random_password()
+        cls.email = generate_random_email()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        # Remove the QR code image after all tests
+        if os.path.exists(cls.qr_code_image_path):
+            os.remove(cls.qr_code_image_path)
+    
+    def tearDown(self):
+        # Add a 1-second delay after each test
+        time.sleep(1)
+
+    def test_home_page(self):
+        self.driver.get(self.host)
+        page_title = self.driver.title
+        print(f"Page title is: {page_title}") 
+        self.assertIn(page_title, self.driver.title)  
+
+    def test_register(self):
+        self.driver.get(self.host + "/register/")  # Registration Page url
+
+        # Fill in the registration form
+        username_field = self.driver.find_element(By.NAME, 'username')
+        email_field = self.driver.find_element(By.NAME, 'email')
+        password_field = self.driver.find_element(By.NAME, 'password')
+        first_name_field = self.driver.find_element(By.NAME, 'first_name')
+        last_name_field = self.driver.find_element(By.NAME, 'last_name')
+    
+        username_field.send_keys(self.username)
+        password_field.send_keys(self.password)
+        email_field.send_keys(self.email)
+        first_name_field.send_keys('Test')
+        last_name_field.send_keys('User')
+
+        # Submit the form
+        submit_button = self.driver.find_element(By.XPATH, '//button[@type="submit"]')
+        submit_button.click()
+
+        # Wait for Sweet Alert modal and confirm
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Setup 2 Factor Authentication"]')))
+        sweet_alert_button = self.driver.find_element(By.XPATH, '//button[text()="Setup 2 Factor Authentication"]')
+        sweet_alert_button.click()
+
+        # Wait for the redirection to the 2FA setup page
+        WebDriverWait(self.driver, 10).until(EC.url_contains('/2fa'))
+
+        # Log in to set up OTP
+        username_field = self.driver.find_element(By.NAME, 'username')
+        password_field = self.driver.find_element(By.NAME, 'password')
+
+        username_field.send_keys(self.username)
+        password_field.send_keys(self.password)
+        password_field.send_keys(Keys.RETURN)
+
+        # Wait for the OTP setup page
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'canvas')))
+
+        # Screenshot of the QR code
+        qr_code_element = self.driver.find_element(By.TAG_NAME, 'canvas')
+        qr_code_element.screenshot(self.qr_code_image_path)
+        
+        try:
+            # Decode the QR code to get the provisioning URI
+            img = cv2.imread(self.qr_code_image_path)
+            decoded_objects = decode(Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)))
+
+            provisioning_uri = None
+            for obj in decoded_objects:
+                if obj.type == 'QRCODE':
+                    provisioning_uri = obj.data.decode('utf-8')
+                    break
+
+            if provisioning_uri is None:
+                raise Exception('QR code not found or not decoded properly.')
+
+            # Extract the secret from the provisioning URI
+            parsed_uri = urlparse.urlparse(provisioning_uri)
+            secret_key = parse_qs(parsed_uri.query)['secret'][0]
+
+            if len(secret_key) % 8 != 0:
+                secret_key += '=' * (8 - len(secret_key) % 8)  # Add padding to the base32 secret key if necessary
+            
+            self.__class__.totp = pyotp.TOTP(secret_key) # Generate the TOTP object using the secret key and store it for later use
+
+            otp = self.totp.now() # Generate the OTP using the secret key
+
+            # Enter the OTP for verification
+            otp_field = self.driver.find_element(By.NAME, 'otpToken')
+            otp_field.send_keys(otp)
+            verify_button = self.driver.find_element(By.XPATH, '//button[@type="submit"]')
+            verify_button.click()
+
+            # Check if the registration and OTP verification were successful
+            WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[text()="OK"]')))
+            sweet_alert_button = self.driver.find_element(By.XPATH, '//button[text()="OK"]')
+            sweet_alert_button.click()
+        except Exception as e:
+            print(f"Error during registration: {e}")
+        finally:
+            # Remove the QR code image after the test if it exists
+            if os.path.exists(self.qr_code_image_path):
+                os.remove(self.qr_code_image_path)
+
+    def test_login(self):
+        if self.totp is None:
+            self.fail("TOTP is not set. Run the registration test first.")
+
+        self.driver.get(self.host + "/login/")  # Login url
+        username = self.username
+        password = self.password
+        
+        # Fill in the login form
+        username_field = self.driver.find_element(By.NAME, 'username')
+        password_field = self.driver.find_element(By.NAME, 'password')
+        otp_field = self.driver.find_element(By.NAME, 'otp')
+
+        username_field.send_keys(username)
+        password_field.send_keys(password)
+
+        # Generate the OTP using the stored TOTP object
+        otp = self.totp.now()
+        otp_field.send_keys(otp)
+        
+        # Submit the form
+        login_button = self.driver.find_element(By.XPATH, '//button[@type="submit"]')
+        login_button.click()
+
+        # Wait for Sweet Alert modal and confirm
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[text()="OK"]')))
+        sweet_alert_button = self.driver.find_element(By.XPATH, '//button[text()="OK"]')
+        sweet_alert_button.click()
+    
+    def test_add_to_cart(self):
+    # Navigate to the home page or the page where the "Add To Cart" button is located
+        self.driver.get(self.host)
+
+        # Find the first "Add To Cart" button and click it
+        add_to_cart_button = self.driver.find_element(By.XPATH, '//button[text()="Add To Cart "]')
+        add_to_cart_button.click()
+
+        # Wait for alert to finish flashing
+        time.sleep(2)
+
+
+    def test_check_cart(self):
+        self.driver.get(self.host+ "/cart/")
+
+    def test_logout(self):
+        self.driver.get(self.host+ "/logout/")
+
+if __name__ == "__main__":
+    suite = unittest.TestSuite()
+    suite.addTest(UITest('test_home_page'))
+    suite.addTest(UITest('test_register'))
+    suite.addTest(UITest('test_login'))
+    suite.addTest(UITest('test_add_to_cart'))
+    suite.addTest(UITest('test_check_cart'))
+    suite.addTest(UITest('test_logout'))
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
