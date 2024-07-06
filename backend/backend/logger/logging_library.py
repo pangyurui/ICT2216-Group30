0bce1c67744cce50285bf4f5614130b50f2dfef0
mechproz <60725331+mechproz@users.noreply.github.com>
2024-07-06 13:10:57 +0800

    updated logging_library.py

--- backend/backend/logger/logging_library.py
+++ backend/backend/logger/logging_library.py
@@ -15,6 +15,7 @@ log_filepath = os.path.join(LOG_DIR, log_filename)
 log_file_index = 1
 MAX_LOG_FILE_SIZE = 5 * 1024 * 1024  # 5MB for example
 
+
 # Function to get the current log file path
 def get_log_filepath():
     global log_filepath, current_log_date, log_file_index
@@ -32,8 +33,10 @@ def get_log_filepath():
     if not os.path.exists(log_filepath):
         open(log_filepath, 'a').close()
 
+
 # Initialize the log file handler
 get_log_filepath()
+
 
 # Function to update the log file handler at midnight
 def update_log_file_handler():
@@ -44,9 +47,11 @@ def update_log_file_handler():
         time.sleep(sleep_time)
         get_log_filepath()
 
+
 # Start a thread to handle log file rotation
 rotation_thread = threading.Thread(target=update_log_file_handler, daemon=True)
 rotation_thread.start()
+
 
 def get_ip_address(request):
     req_headers = request.META
@@ -57,6 +62,7 @@ def get_ip_address(request):
         ip_addr = req_headers.get('REMOTE_ADDR')
     return ip_addr
 
+
 def log_message(message):
     # Ensure the log file exists and check for rotation
     if not os.path.exists(LOG_DIR):
@@ -65,33 +71,42 @@ def log_message(message):
     with open(log_filepath, 'a') as log_file:
         log_file.write(message)
 
+
+def get_formatted_time():
+    current_time = datetime.now().astimezone()
+    formatted_time = current_time.strftime('%Y-%m-%d %H:%M:%S %Z%z')
+    return formatted_time
+
+
 def log_access(request, response, page_name):
     level = "INFO"
     ip_address = get_ip_address(request)
-    access_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    access_time = get_formatted_time()
     response_status = response.status_code
     request_method = request.method
-    user_agent = request.META.get('HTTP_USER_AGENT', '-')
-    content_length = response.get('Content-Length', '-')
-    formatted_message = f'{access_time} {ip_address} [{level}] "{request_method} {page_name} " {response_status} {content_length} "{user_agent}"\n'
+    user_agent = request.headers['User-Agent']
+    content_length = len(response.content)
+    formatted_message = f'{access_time} {ip_address} [{level}] " {request_method} {page_name} " {response_status} {content_length} "{user_agent}"\n'
     log_message(formatted_message)
+
 
 def log_access_register(request, page_name):
     level = "INFO"
     ip_address = get_ip_address(request)
-    access_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    access_time = get_formatted_time()
     request_method = request.method
-    user_agent = request.META.get('HTTP_USER_AGENT', '-')
-    content_length = request.META.get('CONTENT_LENGTH', '-')
-    formatted_message = f'{access_time} {ip_address} [{level}] "{request_method} {page_name} " "{request.data}" {content_length} "{user_agent}"\n'
+    user_agent = request.headers['User-Agent']
+    content_length = len(request.content)
+    formatted_message = f'{access_time} {ip_address} [{level}] "{request_method} {page_name} " "{request.body.decode("utf-8")}" {content_length} "{user_agent}"\n'
     log_message(formatted_message)
+
 
 def log_exception(request, exception):
     level = "ERROR"
     ip_address = get_ip_address(request)
-    access_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    access_time = get_formatted_time()
     request_method = request.method
-    user_agent = request.META.get('HTTP_USER_AGENT', '-')
-    content_length = request.META.get('CONTENT_LENGTH', '-')
+    user_agent = request.headers['User-Agent']
+    content_length = len(request.content)
     formatted_message = f'{access_time} {ip_address} [{level}] "{request_method} {request.path} " "Exception: {exception}" {content_length} "{user_agent}"\n'
     log_message(formatted_message)
